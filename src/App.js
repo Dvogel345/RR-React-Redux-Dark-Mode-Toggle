9b052425d09588f83c8d4b1b9ee474163ea57d4e
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-02-14 21:09:27 -0500

    debugging in progress

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import './App.css';
 import Nav from './components/Nav'
 import ContentWrapper from './components/ContentWrapper'
 import Footer from './components/Footer'
+import { Provider, useSelector, useDispatch } from 'react-redux'
+import { darkMode, lightMode } from './features/modeSlice'
+import {store} from './store'
 
 function App() {
+  const dispatch = useDispatch()
+  const mode = useSelector((state) => state.mode)
+  const toggleMode = () => {
+    mode.darkMode ? dispatch(lightMode()) : dispatch(darkMode())
+  }
   
   return (
-    <div style={{ backgroundColor: 'white', color: 'black' }} className="App">
+    <div style={{ backgroundColor: mode.color1, color: 'white' }} className="App">
+    <Provider store={store}>
       <Nav />
+      <button onClick={toggleMode}>{ mode.darkMode ? 'Light Mode' : 'Dark Mode' }</button>
       <ContentWrapper />
       <Footer />
-    </div>
+    </Provider>  
+  </div>
   );
 }
 
